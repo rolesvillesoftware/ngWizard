7574eca1477d695905ca53a28681f2823039b63a
Bryan Lenihan <bryan@rolesvillesoftware.com>
2018-03-25 16:15:57 -0400

    NPM Errors / fixes

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 export {NgWizardModule} from './src/ng-wizard.module';
-export {NgWizardComponent} from './src/ng-wizard.component';
-export {NgWizardStepComponent} from './src/ng-wizard-step.component';
 export {StepEvent} from './src/StepEvent';
 
 
